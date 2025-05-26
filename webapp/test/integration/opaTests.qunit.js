f2134f76d1222e7a13b1ebb0ac1540da621f9732
Elei Mathew Tatad <elei.mathew.tatad@accenture.com>
2025-05-26 04:34:27 +0000

    initial commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["com/training/exercise1tatad/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"com/training/exercise1tatad/test/unit/controller/MainView.controller"
+], function () {
+	"use strict";
+});
